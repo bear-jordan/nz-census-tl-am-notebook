44b6995691afca2af6e0c71ac3baedd4c4155d76
Bear Jordan <bearjordan@gmail.com>
2023-02-19 19:47:04 +1300

    Sentiment analysis implemented

--- code/change-detection.py
+++ code/change-detection.py
@@ -2,17 +2,16 @@ import pandas as pd
 from detecta import detect_cusum
 import datetime
 
-DATA = pd.read_csv("dummy-data.csv")
-TEST_ID = DATA.iloc[0, 0]
 ID_NAME = "id"
 SIGNAL_NAME = "rating"
 DATE_NAME = "date"
 
-DATA[DATE_NAME] = pd.to_datetime(DATA[DATE_NAME], format="%Y-%m-%d", utc=False)
-
 def select_user(data, uid=1):
     filter = data[ID_NAME] == uid
     return data[filter]
+
+def get_today():
+    return datetime.date.today()
 
 def unique_users(data):
     return data[ID_NAME].unique()
@@ -22,7 +21,7 @@ def change_detection(user_data):
     change, _, _, _ = detect_cusum(signal, threshold=.8, drift=1, ending=False, show=False, ax=None)
     
     resultDf = user_data.loc[:, [ID_NAME, DATE_NAME]].iloc[change]    
-    filter = resultDf[DATE_NAME].dt.date == datetime.date.today()
+    filter = resultDf[DATE_NAME].dt.date == get_today()
     
     if not resultDf[filter].empty:
         return resultDf.loc[filter]
@@ -34,7 +33,9 @@ def main(data):
         todaysResults.append(change_detection(select_user(data, user)))
         
     finalIDs = pd.concat(todaysResults).loc[:, [ID_NAME]]
-    finalIDs.to_csv("to_check.csv", index=False)
+    finalIDs.to_csv("changes-to-check.csv", index=False)
     
 if __name__ == "__main__":
+    DATA = pd.read_csv("dummy-data.csv")
+    DATA[DATE_NAME] = pd.to_datetime(DATA[DATE_NAME], format="%Y-%m-%d", utc=False)
     main(DATA)
---
+++ code/sentiment-analysis.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import datetime
+
+ID_NAME = "id"
+SIGNAL_NAME = "rating"
+TEXT_NAME = "sentiment"
+DATE_NAME = "date"
+SCORE_THRESHOLD = 0.5
+
+def get_today():
+    return datetime.date.today()
+
+def select_user(todaysData, uid=1):
+    filter = todaysData[ID_NAME] == uid
+    return todaysData[filter]
+
+def unique_users(data):
+    return data[ID_NAME].unique()
+
+def load_text(userData):
+    return userData[TEXT_NAME]
+    
+def get_sentiment(text):
+    sentiment = SentimentIntensityAnalyzer()
+    return sentiment.polarity_scores(text)
+    
+    
+def main(data):
+    filterToday = data[DATE_NAME].dt.date == get_today()
+    todaysData = data[filterToday]
+    users = unique_users(todaysData)
+    results = []
+    for user in users:
+        userData = select_user(todaysData, user)
+        text = load_text(userData)
+        score = get_sentiment(text)["neg"]
+        if score > SCORE_THRESHOLD:
+            results.append([user])
+        
+    pd.DataFrame(results, columns=[ID_NAME]).to_csv("sentiments-to-check.csv", index=False)
+        
+if __name__ == "__main__":
+    DATA = pd.read_csv("dummy-data.csv")
+    DATA[DATE_NAME] = pd.to_datetime(DATA[DATE_NAME], format="%Y-%m-%d", utc=False)
+    
+    main(DATA)
